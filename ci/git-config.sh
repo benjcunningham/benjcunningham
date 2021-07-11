2791ff600ceb43eda80271ac5d03f6f55c282a45
Ben Cunningham <benjamescunningham@gmail.com>
2021-07-11 10:45:09 -0400

    Init CI workflows

--- ci/git-config.sh
+++ ci/git-config.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
+git config --global url."https://${GITHUB_TOKEN}@github".insteadOf "https://github"
+
 git config --local user.name "Ben Cunningham"
 git config --local user.email "benjamescunningham@gmail.com"
